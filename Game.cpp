acae6ed9b64e3b0163cb9d12a9e8f40e0a7f7381
Muhammad Haroon <mharoon1326@gmail.com>
2023-01-25 18:12:02 +0500

    Tic Tac Toe

--- Game.cpp
+++ Game.cpp
@@ -9,24 +9,45 @@ using namespace std;
 int gd = DETECT, gm;
 
 
-int choice_x, choice_y,k;
+int choice_x, choice_y,k,player = 1,x,y,i;
 char choice[50];
 char mark[1],array[10];
 POINT pos;
 
+int play_game();
 int win();
+int Congrats_Winner();
 int draw_board();
 int main()
 {
-	
 	initgraph(&gd, &gm, 0);
+	play_game();
+	if(k==1)
+	{
+		Congrats_Winner();
+	}
+	getch();
+}
+
+int draw_board()
+{
+	bar(503, 50, 509, 480);
+	bar(770, 50, 776, 480);
+	bar(370, 170, 900, 176);
+	bar(370, 335, 900, 341);
+	//SQUARE
+	bar(370, 50, 900, 56);
+	bar(370, 50, 376, 480);
+	bar(370, 480, 900, 486);
+	bar(900, 50, 906, 480);
+}
+int play_game()
+{
 	initwindow(1275, 1000, "Tic Tac Toe");
-	setcolor(YELLOW);
 	int w = textwidth("Tic Tac Toe");
 	settextstyle(10, 0, 5);
 	outtextxy(500, 2, "Tic Tac Toe");
-	int player = 1 ;
-
+	
 	do
 	{
 		draw_board();
@@ -35,12 +56,10 @@ int main()
 		
 		if(player==1)
 		{
-			setcolor(GREEN);
 			mark[0]='X';
 		}
 		else if(player==2)
 		{
-			setcolor(BROWN);
 			mark[0]='O';
 		}
 		 if (!kbhit())
@@ -203,34 +222,14 @@ int main()
 		k= win();
 		if(k==1)
 		{
-			getch();
+			delay(900);
 		}
 		
 	} 
 	while(k==-1);
-	if(k==1)
-	{
-		cout<<"thanks";
-	}
-}
-
-int draw_board()
-{
-	setcolor(BLUE);
-	bar3d(503, 50, 509, 480, 5, 1);
-	bar3d(770, 50, 776, 480, 5, 1);
-	bar3d(370, 170, 900, 176, 5, 1);
-	bar3d(370, 335, 900, 341, 5, 1);
-	//SQUARE
-	setcolor(RED);
-	bar3d(370, 50, 900, 56,5,1);
-	bar3d(370, 50, 376, 480,5,1);
-	bar3d(370, 480, 900, 486,5,1);
-	bar3d(900, 50, 906, 480,5,1);
 }
 int win()
 {
-	setcolor(CYAN);
 //	rows
 	//first row
 	
@@ -239,12 +238,10 @@ int win()
 		if((array[1]==1)&&(array[2]==2)&&(array[3]==3))
 		{
 		
-			settextstyle(1, 0, 5);
 			outtextxy(500, 500, "X WIN");
 		}
 		else
 		{
-			settextstyle(10, 0, 5);
 			outtextxy(500, 500, "O WIN ");
 		}
 		return 1;
@@ -255,12 +252,10 @@ int win()
 		if((array[4]==4)&&(array[5]==5)&&(array[6]==6))
 		{
 		
-			settextstyle(10, 0, 5);
 			outtextxy(500, 500, "X WIN");
 		}
 		else
 		{
-			settextstyle(10, 0, 5);
 			outtextxy(500, 500, "O WIN");
 		}
 		return 1;
@@ -271,12 +266,10 @@ int win()
 		if((array[7]==7)&&(array[8]==8)&&(array[9]==9))
 		{
 		
-			settextstyle(10, 0, 5);
 			outtextxy(500, 500, "X WIN");	
 		}
 		else
 		{
-			settextstyle(10, 0, 5);
 			outtextxy(500, 500, "O WIN");
 		}
 		return 1;
@@ -288,12 +281,10 @@ int win()
 		if((array[1]==1)&&(array[4]==4)&&(array[7]==7))
 		{
 		
-			settextstyle(10, 0, 5);
 			outtextxy(500, 500, "X WIN");
 		}
 		else
 		{
-			settextstyle(10, 0, 5);
 			outtextxy(500, 500, "O WIN");
 		}
 		return 1;
@@ -304,12 +295,10 @@ int win()
 		if((array[2]==2)&&(array[5]==5)&&(array[8]==8))
 		{
 		
-			settextstyle(10, 0, 5);
 			outtextxy(500, 500, "X WIN");
 		}
 		else
 		{
-			settextstyle(10, 0, 5);
 			outtextxy(500, 500, "O WIN");
 		}
 		return 1;
@@ -320,12 +309,10 @@ int win()
 		if((array[3]==3)&&(array[6]==6)&&(array[9]==9))
 		{
 		
-			settextstyle(10, 0, 5);
 			outtextxy(500, 500, "X WIN");
 		}
 		else
 		{
-			settextstyle(10, 0, 5);
 			outtextxy(500, 500, "O WIN");
 		}
 		return 1;
@@ -337,12 +324,10 @@ int win()
 		if((array[1]==1)&&(array[5]==5)&&(array[9]==9))
 		{
 		
-			settextstyle(10, 0, 5);
 			outtextxy(500, 500, "X WIN");
 		}
 		else
 		{
-			settextstyle(10, 0, 5);
 			outtextxy(500, 500, "O WIN");
 		}
 		return 1;
@@ -352,12 +337,10 @@ int win()
 		if((array[3]==3)&&(array[5]==5)&&(array[7]==7))
 		{
 		
-			settextstyle(10, 0, 5);
 			outtextxy(500, 500, "X WIN");
 		}
 		else
 		{
-			settextstyle(10, 0, 5);
 			outtextxy(500, 500, "O WIN");
 		}
 		return 1;
@@ -366,69 +349,16 @@ int win()
 	{
 		return -1;
 	}
-
 }
-		
-		
-		
-		
-//		if(GetAsyncKeyState(VK_LBUTTON))
-//		{
-//			GetCursorPos(&pos);
-//			//FIRST_ROW
-//			if ((pos.x < 500) && (pos.x > 370) && (pos.y < 170) && (pos.y > 50))
-//			{
-//				choice_x = 410;
-//				choice_y = 62;
-//				
-//				sprintf(mark, "%c", mark[0]);
-//				outtextxy(choice_x, choice_y, mark);
-//				if(mark[0]=='X')
-//				{
-//					array[1]=1;
-//				}
-//				else if(mark[0]=='O')
-//				{
-//					array[1]=11;
-//				}
-//				player=(player==1)?2:1;
-//			}
-//			else if ((pos.x < 770) && (pos.x > 500) && (pos.y < 170) && (pos.y > 50))
-//			{
-//				choice_x = 610;
-//				choice_y = 62;
-//				sprintf(mark, "%c", mark[0]);
-//				outtextxy(choice_x, choice_y, mark);
-//				if(mark[0]=='X')
-//				{
-//					array[2]=2;
-//				}
-//				else
-//				{
-//					array[2]=12;
-//				}
-//				player=(player==1)?2:1;
-//			}
-//			else if ((pos.x < 900) && (pos.x > 770) && (pos.y < 170) && (pos.y > 50))
-//			{
-//				choice_x = 810;
-//				choice_y = 62;
-//				sprintf(mark, "%c", mark[0]);
-//				outtextxy(choice_x, choice_y, mark);
-//				if(mark[0]=='X')
-//				{
-//					array[3]=3;
-//				}
-//				else
-//				{
-//					array[3]=13;
-//				}
-//				player=(player==1)?2:1;
-//			}
-//			//SECOND_ROW
-			
-			
-			
-//			
-//		}
+int Congrats_Winner()
+{
+	initwindow(1275, 1000, "Congrats Winner");
+	
+    settextstyle(10, 0, 9);
+    setcolor(CYAN);
+    outtextxy(170, 250, "Congratulations");
+    outtextxy(1030, 250, mark);
+    setbkcolor(YELLOW);
+    getch();
+}
 
